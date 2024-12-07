f5e711dbb923b3c34a298b9c2e684bdd115ad6df
Levente <145211298+fulopl@users.noreply.github.com>
2024-12-07 12:27:16 +0100

    replace Tiles funcionality to TileType

--- src/main/java/com/codecool/cheeseterminator/ui/TileType.java
+++ src/main/java/com/codecool/cheeseterminator/ui/TileType.java
@@ -1,5 +1,9 @@
 package com.codecool.cheeseterminator.ui;
 
+import com.codecool.cheeseterminator.data.Drawable;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
 public enum TileType {
     EMPTY("empty", false, 0, 0, ' '),
     FLOOR("floor", true, 2, 0, '.'),
@@ -11,16 +15,32 @@ public enum TileType {
 
     private final String tileName;
     private final boolean passable;
-    private final int imgCoordinateX;
-    private final int imgCoordinateY;
+    private final int columnIndex;
+    private final int rowIndex;
     private final char mapCharacter;
+    private final int x;
+    private final int y;
 
-    TileType(String tileName, boolean passable, int imgCoordinateX, int imgCoordinateY, char mapCharacter) {
+    public static final int TILE_WIDTH = 32;
+    private static final Image tileset = new Image("/tiles.png", 543 * 2, 543 * 2, true, false);
+
+    TileType(String tileName, boolean passable, int columnIndex, int rowIndex, char mapCharacter) {
         this.tileName = tileName;
         this.passable = passable;
-        this.imgCoordinateX = imgCoordinateX;
-        this.imgCoordinateY = imgCoordinateY;
+        this.columnIndex = columnIndex;
+        this.rowIndex = rowIndex;
         this.mapCharacter = mapCharacter;
+        x = columnIndex * (TILE_WIDTH + 2);
+        y = rowIndex * (TILE_WIDTH + 2);
+    }
+
+    public static void drawTile(GraphicsContext context, Drawable drawable, int x, int y) {
+        TileType tileType = TileType.valueOf(drawable.getTileName().toUpperCase());
+        context.drawImage(tileset
+                , tileType.getX(), tileType.getY()
+                , TILE_WIDTH, TILE_WIDTH
+                ,x * TILE_WIDTH, y * TILE_WIDTH
+                , TILE_WIDTH, TILE_WIDTH);
     }
 
     public String getTileName() {
@@ -31,15 +51,23 @@ public enum TileType {
         return passable;
     }
 
-    public int getImgCoordinateX() {
-        return imgCoordinateX;
+    public int getColumnIndex() {
+        return columnIndex;
     }
 
-    public int getImgCoordinateY() {
-        return imgCoordinateY;
+    public int getRowIndex() {
+        return rowIndex;
     }
 
     public char getMapCharacter() {
         return mapCharacter;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
