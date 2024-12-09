b00aad4052a218017250aba032080c1346137e6a
Levente <145211298+fulopl@users.noreply.github.com>
2024-12-09 12:27:20 +0100

    make Direction a smart enum

--- src/main/java/com/codecool/cheeseterminator/data/player/Direction.java
+++ src/main/java/com/codecool/cheeseterminator/data/player/Direction.java
@@ -1,5 +1,11 @@
 package com.codecool.cheeseterminator.data.player;
 
 public enum Direction {
-    NORTH, EAST, SOUTH, WEST;
+    NORTH(0, -1), EAST(1, 0), SOUTH(0, 1), WEST(-1, 0);
+
+    int dx;
+    int dy;
+
+    Direction(int dx, int dy) {
+    }
 }
