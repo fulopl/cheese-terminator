08df4f91ec0230de2640ebbd185c7d671343adc2
Levente <145211298+fulopl@users.noreply.github.com>
2024-12-09 15:14:31 +0100

    fix constructor in Direction.java

--- src/main/java/com/codecool/cheeseterminator/data/player/Direction.java
+++ src/main/java/com/codecool/cheeseterminator/data/player/Direction.java
@@ -3,9 +3,19 @@ package com.codecool.cheeseterminator.data.player;
 public enum Direction {
     NORTH(0, -1), EAST(1, 0), SOUTH(0, 1), WEST(-1, 0);
 
-    int dx;
-    int dy;
+    final int dx;
+    final int dy;
 
     Direction(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public int getDx() {
+        return dx;
+    }
+
+    public int getDy() {
+        return dy;
     }
 }
