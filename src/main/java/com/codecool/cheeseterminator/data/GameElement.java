d12e6b84e84e96c435dc45c700e12eed46a23189
Levente <145211298+fulopl@users.noreply.github.com>
2024-12-09 15:16:03 +0100

    add GameElementType field to GameElement.java

--- src/main/java/com/codecool/cheeseterminator/data/GameElement.java
+++ src/main/java/com/codecool/cheeseterminator/data/GameElement.java
@@ -8,16 +8,26 @@ public class GameElement implements Drawable{
     private final char mapCharacter;
     private Tile tile;
     protected Cell cell;
+    private final GameElementType gameElementType;
 
     public GameElement(GameElementType gameElementType) {
         this.passable = gameElementType.isPassable();
         this.role = gameElementType.getRole();
         this.mapCharacter = gameElementType.getMapCharacter();
         this.tile = gameElementType.getInitialTile();
+        this.gameElementType = gameElementType;
     }
 
     @Override
     public Tile getTile() {
         return tile;
+    }
+
+    public boolean isPassable() {
+        return passable;
+    }
+
+    public GameElementType getGameElementType() {
+        return gameElementType;
     }
 }
