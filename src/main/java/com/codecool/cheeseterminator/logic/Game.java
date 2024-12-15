63c723b55b5ca21e4ef0f2f39ab7d629a2fd5374
Levente <145211298+fulopl@users.noreply.github.com>
2024-12-15 15:10:28 +0100

    remove unused imports

--- src/main/java/com/codecool/cheeseterminator/logic/Game.java
+++ src/main/java/com/codecool/cheeseterminator/logic/Game.java
@@ -1,16 +1,12 @@
 package com.codecool.cheeseterminator.logic;
 
 import com.codecool.cheeseterminator.ui.UI;
-import com.codecool.cheeseterminator.ui.keyeventhandler.*;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
-import java.util.Set;
 
 public class Game extends Application {
     private UI ui;
     private GameLogic logic;
-    private Set<KeyHandler> keyHandlers;
     private InputManager inputManager;
 
     public static void main(String[] args) {
